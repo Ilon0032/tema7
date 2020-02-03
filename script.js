c958f3746c4654ec46b8bc51c79fb26256f967b0
Luna Baunkjær <lunabaunkjaer@Lunas-MacBook-Pro.local>
2020-02-03 10:29:45 +0100

    areal first

---
+++ script.js
@@ -0,0 +1,7 @@
+let længde = 3;
+let bredde = 5;
+let areal = længde * bredde;
+
+let resultat = `Længden er ${længde} meter og bredden er ${bredde} arealtet er ${areal} kvardratmeter`
+
+console.log(resultat);
